cfec4a16f5e818f3c4e5a754e3af6416400a8364
Hamsa Omar <hamsaomar@infra04-wg012.eduroam.wireless.umn.edu>
2023-02-15 12:39:54 -0600

    moved cors implementation

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5001;
 
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000 })
+);
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://www.modeltune.co"); // update to match the domain you will make the request from
   res.header(
@@ -28,11 +33,6 @@ app.use(function (req, res, next) {
 //   );
 //   next();
 // });
-
-app.use(express.json({ limit: "50mb" }));
-app.use(
-  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000 })
-);
 
 const prisma = require("./prisma.js");
 
