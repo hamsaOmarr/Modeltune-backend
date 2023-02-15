6acccd07306e29263be6b97e9bd9c690ab1e3197
Hamsa Omar <hamsaomar@infra04-wg012.eduroam.wireless.umn.edu>
2023-02-15 11:58:09 -0600

    cors added

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5001;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000 })
