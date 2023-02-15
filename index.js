c1dfd45d67a81f4cb421c65513d390fa9fc2e4e0
Hamsa Omar <hamsaomar@infra04-wg012.eduroam.wireless.umn.edu>
2023-02-15 12:06:50 -0600

    changed cors middleware

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
@@ -26,7 +28,6 @@ prisma
   .then(() => console.log("Prisma Connection established"))
   .catch((err) => console.log(err));
 
-app.use(cors());
 
 const indexRouter = require("./routes/index.js");
 
