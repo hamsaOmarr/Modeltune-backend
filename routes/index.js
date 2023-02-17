7d5604b37b41b214af5a7989d9a1ac77b3bee63d
hamsaOmarr <hamsa@modeltune.co>
2023-02-17 09:08:30 -0600

    made email send async

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   await prisma.email
     .create({ data: { email: req.body.Email } })
-    .then(() => {
+    .then(async () => {
       res.json("Thank you! Your submittion has been received!");
 
       const msg = {
@@ -45,7 +45,7 @@ Hamsa Omar
 Founder at Modeltune`,
       };
 
-      sgMail
+      await sgMail
         .send(msg)
         .then(() => {
           console.log("Email sent");
